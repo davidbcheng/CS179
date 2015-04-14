4948f5bf8b7051e811487097f2c4a1efc8a539b3
David Cheng <dcheng@caltech.edu>
2015-04-14 01:21:35 -0700

    debug

--- Set2/transpose_cuda.cu
+++ Set2/transpose_cuda.cu
@@ -102,7 +102,7 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
 
   for (j = init_j; j < end_j; j++) {
     // output[i + n * j] = data[data_j + 65 * data_i];
-    output[j + n * j] = data[data_i + 65 * data_j];
+    output[j + n * i] = data[data_i + 65 * data_j];
   }
   
 }
