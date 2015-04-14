744222b92f8273fb4bc4d4b586c7c3437660cb16
David Cheng <dcheng@caltech.edu>
2015-04-14 01:38:43 -0700

    debug

--- Set2/transpose.cc
+++ Set2/transpose.cc
@@ -28,24 +28,24 @@ inline void gpuAssert(cudaError_t code,
   }
 }
 
-// a and b point to n x n matrices. This method checks that A = B^T.
-void checkTransposed(const float *a, const float *b, int n) {
-  bool correct = true;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (a[i + n * j] != b[j + n * i]) {
-        correct = false;
+// // a and b point to n x n matrices. This method checks that A = B^T.
+// void checkTransposed(const float *a, const float *b, int n) {
+//   bool correct = true;
+//   for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < n; j++) {
+//       if (a[i + n * j] != b[j + n * i]) {
+//         correct = false;
 
-        fprintf(stderr, "Transpose failed: a[%d, %d] != b[%d, %d], %f != %f\n",
-                i, j, j, i, a[i + n * j], b[j + n * i]);
+//         fprintf(stderr, "Transpose failed: a[%d, %d] != b[%d, %d], %f != %f\n",
+//                 i, j, j, i, a[i + n * j], b[j + n * i]);
 
-        assert(correct);
-      }
-    }
-  }
+//         assert(correct);
+//       }
+//     }
+//   }
 
-  assert(correct);
-}
+//   assert(correct);
+// }
 
 // a and b point to n x n matrices. This method checks that A = B^T.
 void checkTransposed(const float *a, const float *b, int n) {
