a1918d674045c77fb16c2b9c6029261e73196019
David Cheng <dcheng@caltech.edu>
2015-04-30 16:16:48 -0700

    Implemented kernels

--- Set4/xray_ct_recon.cu
+++ Set4/xray_ct_recon.cu
@@ -50,8 +50,64 @@ void checkCUDAKernelError()
 
 }
 
+__global__
+void
+cudaHighPassKernel(cufftComplex *raw_data, int length) {
+    // Determine the index of the output data we are writing to by
+    // the block id and the thread id
+    unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
 
+    while(index < length)
+    {
+        raw_data[index].x = fabsf(1 - (2 * index) / length));
+        raw_data[index].y = fabsf(1 - (2 * index) / length));
 
+        index += blockDim.x + gridDim.x;
+    }
+}
+
+__global__
+void
+cudaBackProjectionKernel(float *sinogram, float *result, int size_result,
+    int nAngles, int sinogram_width, int side_length) {
+
+    int x_p = threadIdx.x + blockDim.x * blockIdx.x;
+    int y_p = threadIdx.y + blockDim.y * blockIdx.y;
+    float x_0 = (float) x_p - (side_length / 2);
+    float y_0 = (side_length / 2) - (float) y_p;
+
+    int angle;
+
+    for (int angle = 0; angle < nAngles; ++angle)
+    {
+        float theta = ((float) angle / nAngles) * PI;
+        float d;
+        if (theta == 0)
+        {
+            d = x_0;
+        }
+        else if (theta == PI / 2)
+        {
+            d = y_0;
+        }
+        else
+        {
+            float m = -1 * cosf(theta) / sinf(theta);
+            float q = -1 / m;
+
+            float x_1 = (y_0 - m * x_0) / (q - m);
+            float y_1 = q * x_1;
+            d = sqrtf(x_1 * x_1 + y_1 * y_1);
+
+            if (x_1 < 0 || (q < 0 && x_1 > 0))
+            {
+                d = -1 * d;
+            }
+        }
+
+        result[x_p + side_length * y_p] += sinogram[(int) d + nAngles * angle];
+    }
+}
 
 int main(int argc, char** argv){
 
@@ -133,6 +189,10 @@ u
     cudaMalloc((void **) &output_dev, size_result);
     cudaMalloc((void **) &dev_sinogram_float, sinogram_width*nAngles*sizeof(float));
 
+    cudaMemcpy(dev_sinogram_float, sinogram_host,
+        sizeof(cufftComplex) * sinogram_width * nAngles, cudaMemcpyHostToDevice);
+
+
 
     /* TODO 1: Implement the high-pass filter:
         - Use cuFFT for the forward FFT
@@ -145,6 +205,26 @@ u
         transforms in cuFFT, you'll have to slightly change our code above.
     */
 
+    cufftHandle plan;
+    cufftHandle plan2;
+    int batch = nAngles;
+    cufftPlan1d(&plan, sinogram_width*nAngles*sizeof(cufftComplex),
+        CUFFT_C2C, batch);
+    cufftPlan1d(&plan2, sinogram_width*nAngles*sizeof(cufftComplex),
+        CUFFT_C2R, batch);
+
+
+    cufftExecC2C(&plan, dev_sinogram_cmplx, dev_sinogram_cmplx, CUFFT_FORWARD);
+
+    cudaHighPassKernel<<<nBlocks, threadsPerBlock>>> (dev_sinogram_cmplx,
+        sinogram_width*nAngles*sizeof(cufftComplex));
+
+    cufftExecC2R(&plan2, dev_sinogram_float, dev_sinogram_cmplx, CUFFT_INVERSE);
+    cufftDestroy(plan);
+    cufftDestroy(plan2);
+
+    cudaFree(dev_sinogram_cmplx);
+
 
     /* TODO 2: Implement backprojection.
         - Allocate memory for the output image.
@@ -153,7 +233,15 @@ u
         - Free all remaining memory on the GPU.
     */
 
-    
+    cudaMalloc((void **) &output_dev, size_result);
+    cudaBackProjectionKernel <<<nBlocks, threadsPerBlock>>> (dev_sinogram_float,
+     output_dev, size_result, nAngles, sinogram_width, length);
+    cudaMemcpy(output_host, output_dev, size_result, cudaMemcpyDeviceToHost);
+
+    cudaFree(dev_sinogram_float);
+    cudaFree(output_dev);
+    cudaFree(dev)
+
     /* Export image data. */
 
     for(j = 0; j < width; j++){
